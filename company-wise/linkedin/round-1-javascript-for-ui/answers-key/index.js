8b606afd92a924863cac8b26f96de8b637e079ed
Abhinav Gupta <abhinavgupta@Abhinavs-MacBook-Pro.local>
2025-03-26 22:44:55 +0530

    [linkedin] partial answer key

---
+++ company-wise/linkedin/round-1-javascript-for-ui/answers-key/index.js
@@ -0,0 +1,11 @@
+class Calculator {
+  constructor() {
+    this.inputArr = [];
+  }
+
+  init() {
+    // use event delegation when attaching event listeners
+    // event listener only needs to be attached to the container of buttons
+    // use button type and innerHTML to handle numbers vs operators
+  }
+}
